e8350aecb42199edc3b712e6ab6c0e606b8c2253
David Thai <davidthai99@gmail.com>
2020-11-28 17:25:17 -0500

    fixed all errors

--- comp345_project/GameEngine.cpp
+++ comp345_project/GameEngine.cpp
@@ -12,9 +12,10 @@
 #include <fstream>
 #include <vector>
 #include <errno.h>
+#include <random>
 
 using namespace std;
-
+namespace fs = std::filesystem;
 GameEngine::GameEngine() {
     nbOfPlayers = 0;
     deck = new Deck();
@@ -82,7 +83,7 @@ void GameEngine::GameStart()
             string path = "conquest/";
             int fileCount = 1;
             for (const auto & entry : std::filesystem::directory_iterator(path)) {
-                string mapChoice = entry.path();
+                string mapChoice = entry.path().string();
                 int pos = mapChoice.find("/");
                 mapChoice = mapChoice.substr(pos);
                 mapChoice.erase(0,1);
@@ -127,7 +128,7 @@ void GameEngine::GameStart()
             string path = "maps/";
             int fileCount = 1;
             for (const auto & entry : std::filesystem::directory_iterator(path)) {
-                string mapChoice = entry.path();
+                string mapChoice = entry.path().string();
                 int pos = mapChoice.find("/");
                 mapChoice = mapChoice.substr(pos);
                 mapChoice.erase(0,1);
@@ -178,7 +179,10 @@ void GameEngine::GameStart()
         cin >> name;
         players.push_back(new Player(name));
     }
-    random_shuffle(players.begin(), players.end());     // shuffling the list of players
+
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(players.begin(), players.end(), g);     // shuffling the list of players
     cout << endl;
 }
 string GameEngine::selectMapType()
@@ -471,7 +475,9 @@ string GameEngine::getPhase(){
 void GameEngine::startupPhase() {
     cout << endl << "<<<<<<<<<< startUpPhase() START" << endl;
     vector<Country*> randomCountries = (*gameMap).getCountries();
-    random_shuffle(randomCountries.begin(), randomCountries.end());     // shuffling the list of countries
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(randomCountries.begin(), randomCountries.end(), g);     // shuffling the list of countries
     int c = 0;
     for (Country* country : randomCountries) {      // assigning each country to a player
         (*players[c]).setCountry(country);
