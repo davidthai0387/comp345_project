b992d496c286c442b23eeaf7ddf5e692fa123bf0
Jason <jason.k99@hotmail.com>
2020-11-28 21:46:46 -0500

    humanstrat toDefend()

--- comp345_project/PlayerStrategies.cpp
+++ comp345_project/PlayerStrategies.cpp
@@ -16,26 +16,33 @@ vector<Country*> HumanPlayer::toAttack(Player* p) {
 	int counter = 1;
 	int choice;
 
-	// Printing confirmed countries if there are any
-	if (confirmed.size() != 0) {
-		cout << "\nHere is the list of countries you would like to attack:" << endl;
-		for (Country* country : confirmed) {
-			cout << "To Attack: " << country->getName() << endl;
-		}
-	}
-	// Printing bordering countries
-	cout << "Here is a list of bordering countries:" << endl;
+	
+	// Potential list initialization
 	for (Country* country : p->getOwnedCountries()) {
-		for (Country* border : country->getBorders()) {
-			cout << "Country #" << counter << " " << border->getName() << endl;
-			counter++;
-		}
 		potential.insert(potential.end(), country->getBorders().begin(), country->getBorders().end());
 	}
-	
-	// Player choice
+
 	while (potential.size() > 0) {
 		try {
+			// Printing confirmed countries if there are any
+			if (confirmed.size() != 0) {
+				cout << "\nList of countries to attack:" << endl;
+				int confirmedcounter = 1;
+				for (Country* country : confirmed) {
+					cout << "Country #" << confirmedcounter << "\t" << country->getName() << endl;
+				}
+				cout << endl;
+			}
+
+			// Printing attacking option list
+			cout << "Here is a list of bordering countries:" << endl;
+			int counter = 1;
+			for (Country* country : potential) {
+				cout << "Country #" << counter << "\t" << country->getName() << endl;
+				counter++;
+			}
+
+			// Player choice
 			if (confirmed.size() >= 1) {
 				cout << "If you want to stop adding more countries, enter -1 in the choice." << endl;
 			}
@@ -65,9 +72,61 @@ vector<Country*> HumanPlayer::toAttack(Player* p) {
 
 vector<Country*> HumanPlayer::toDefend(Player* p) {
 
-	// TODO
+	// Initalize variables
+	vector<Country*> potential;
+	vector<Country*> confirmed;
+	int counter = 1;
+	int choice;
 
-	return vector<Country*>();
+	// Potential list initialization
+	potential.insert(potential.end(), p->getOwnedCountries().begin(), p->getOwnedCountries().end());
+
+	while (potential.size() > 0) {
+		try {
+			// Printing confirmed countries if there are any
+			if (confirmed.size() != 0) {
+				cout << "\nThe list of countries to defend:" << endl;
+				int confirmedcounter = 1;
+				for (Country* country : confirmed) {
+					cout << "Country #" << confirmedcounter << "\t" << country->getName() << endl;
+				}
+				cout << endl;
+			}
+
+			// Printing defending option list
+			cout << "Here is a list of your owned countries:" << endl;
+			int counter = 1;
+			for (Country* country : potential) {
+				cout << "Country #" << counter << "\t" << country->getName() << endl;
+				counter++;
+			}
+
+			// Player choice
+			if (confirmed.size() >= 1) {
+				cout << "If you want to stop adding more countries, enter -1 in the choice." << endl;
+			}
+			cout << "Please enter the number of a country that you would like to defend: ";
+			cin >> choice;
+			if (choice > 0) {
+				choice -= 1;
+				confirmed.push_back(potential[choice]);
+				potential.erase(potential.begin() + choice);
+				cout << "Your choice has been confirmed." << endl;
+			}
+			else if (confirmed.size() >= 1 && choice == -1) {
+				cout << "You have decided not to attack any more countries. The confirmed countries will be defend." << endl;
+				break;
+			}
+			else {
+				cout << "That was not a valid choice. Please try again..." << endl;
+				continue;
+			}
+		}
+		catch (exception e) {
+			e.what();
+		}
+	}
+	return confirmed;
 }
 
 void HumanPlayer::playCard(Player* p, vector<Player*> o, Deck* d, Map* m) {
@@ -106,6 +165,10 @@ void AggressiveComputer::issueOrder(string orderName, Player* p, vector<Player*>
 vector<Country*> AggressiveComputer::toAttack(Player* p) {
 
 	// TODO 
+	vector<Country*> potential;
+	vector<Country*> confirmed;
+
+
 
 	return vector<Country*>();
 }
